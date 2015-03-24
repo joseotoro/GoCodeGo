2064b509d85f87e4866b8612ca8c7831034bb7f7
Nur AG <nurloco@hotmail.com>
2015-03-24 20:37:00 +0100

    Recovery: compile/run Golang programs
    'Assertion' code for GO and the compilation/process managing for problem
    solving attempt sessions.

--- gocodego/go.py
+++ gocodego/go.py
@@ -2,6 +2,11 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.decorators import login_required
 
+import subprocess
+import time
+import os
+import uuid
+
 from models import Problem, ProblemSolution
 
 @login_required
@@ -19,3 +24,95 @@ def check(request):
         return HttpResponse('asdads')
     else:
         return HttpResponse('')
+
+def compile_go(source_file, target_file):
+    # Clean up the target file if it exists
+    if os.path.isfile(target_file):
+        os.remove(target_file)
+
+    # Run the GO process
+    proc = subprocess.Popen(['go', 'build', '-o', target_file, source_file], stdout=subprocess.PIPE)
+    out = proc.communicate()[0]
+
+    # Check if the target file has been created successfully
+    if not os.path.isfile(target_file):
+        return out
+    else:
+        return True
+
+def run_timeout(program_path, args, timeout_limit):
+    # Try running the program
+    proc = subprocess.Popen([program_path]+args, stdout=subprocess.PIPE)
+
+    # Wait until the timeout time is reached
+    time_elapsed = 0
+    while proc.poll() is None and time_elapsed < timeout_limit:
+        time.sleep(1)
+        time_elapsed += 1
+
+    # Kill the process if it is still alive
+    # since it should've finished already
+    if proc.poll() is None:
+        proc.terminate()
+        return 'ERROR: Your code got timed out! You need to Git Gud with GO!'
+
+    # Get the thing if we haven't timed out
+    return proc.communicate()[0]
+
+def session_comp_run(input_code, base_code):
+    session = str(uuid.uuid4())
+
+    # Produce the temporary code file
+    code_file = session + '.go'
+    code = file(code_file, 'w')
+    code.write(base_code + '\n' + input_code)
+    code.close()
+
+    # Compile the program and delete the code
+    executable = session + ".exe"
+    result = compile_go(code_file, executable)
+    os.remove(code_file)
+
+    # If the code didn't compile, put out why
+    if type(result)=='str':
+        return result
+
+    # Run the code with a timeout
+    # and return its result
+    result = run_timeout(executable, [], 10)
+    os.remove(executable)
+
+    return result
+
+func = '''
+        func add(a int, b int) int{
+            return a + b
+        }
+        '''
+
+base = '''
+package main
+        import "fmt"
+
+        /*
+         * INPUT: value, result, expected
+         *
+         * BOOM if result != expected
+         */
+        func assert(result interface{}, expected interface{}){
+            if(result != expected){
+                panic(fmt.Sprintf("FAILED! Yielded %v, but expected %v as the result", result, expected))
+            }
+        }
+
+        func main() {
+            assert(add(1,1), 2)
+            //assert(add(1,2), 2)
+        }
+        '''
+print(session_comp_run(func, base))
+
+#source = 'code/hello.go'
+#target = 'asdf.exe'
+#print(compile_go(source, target))
+#print(run_timeout(target, [], 4))
