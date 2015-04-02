38b7f23dce18d3c78d5ea4f3fed1b4f2de30ea52
Jose Toro <wynkth@gmail.com>
2015-04-02 20:08:42 +0200

    Fixed problem url bug

--- gocodego/urls.py
+++ gocodego/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # Problems
     url(r'^problems/$', views.problems, name='problems'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^problems/(\d)$', views.detail, name='detail'),
+    url(r'^problems/(\d+)$', views.detail, name='detail'),
     url(r'^problems/check_solution/$', go.check, name='check_solution'),
     url(r'^problems/save_solution/$', go.save, name='save_solution'),
 
